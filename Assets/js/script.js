127a7ac5d7a5ee086bbaeb54261849b897005d5f
Daria <dariabatiuk5@gmail.com>
2022-11-24 23:19:24 -0400

    created var for local storadge

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -37,12 +37,17 @@ changeStyle ();
 
   // TODO: Add a listener for click events on the save button. 
   $(".saveBtn").on("click", function () {
+    //This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. 
+    let time = $(this).parent().attr("id");
+    let text = $(this).siblings(".description").val();
+    
     
 });
   
-  //This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  
+  //HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
