e289844c0d2a335428c9e56f9bdd48fdbe6ba635
Daria <dariabatiuk5@gmail.com>
2022-11-24 22:40:02 -0400

    Added time variables, loop for css styles, onclick function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,17 +1,54 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDate = moment().format('dddd, MMM Do YYYY');
+
+
+//shows the current time
+let currentDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDate);
 
+let currentTime = moment().hour()
+
+let stopTime = parent($(this).attr("id"));
+
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+    //function for changing the css style
+function changeStyle () {
+    if (stopTime < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (stopTime === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+
+    }
+}
+changeStyle ();
+
+  // TODO: Add a listener for click events on the save button. 
+  $(".saveBtn").on("click", function () {
+    
+});
+  
+  //This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
