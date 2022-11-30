8913088e7912a9ae19345f217c97d79a1684360a
Daria <dariabatiuk5@gmail.com>
2022-11-30 16:42:23 -0400

    Done Js for LS. Need to rewrite this into JQ

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,68 +1,70 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 //shows the current time
-let currentDate = moment().format('dddd, MMM Do YYYY');
+
+let currentDate = dayjs().format("dddd, MMM D YYYY");
 $("#currentDay").html(currentDate);
-
-let currentTime = moment().hour()
-
-let stopTime = parent($(this).attr("id"));
-
+let hour = document.querySelectorAll(".description");
+let hour9 = document.getElementById("h9");
+console.log(JSON.parse(localStorage.getItem("taskObject")).hour10);
 
 
-$(function () {
-    //function for changing the css style
-function changeStyle () {
-    if (stopTime < currentTime) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
-    }
-    else if (stopTime === currentTime) {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
-    }
-    else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
 
-    }
-}
-changeStyle ();
+hour9.value = JSON.parse(localStorage.getItem("taskObject")).hour9;
 
-  // TODO: Add a listener for click events on the save button. 
-  $(".saveBtn").on("click", function () {
-    //This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
+let currentTime = dayjs().format("hh:mm");
+
+// function initilizeLocalStorage() {
+//   // getting data from LS
+//   const taskObjInLocalStorage = JSON.parse(localStorage.getItem("taskObject")) || {};
+
+//   // console.log(objDescrNodes, "objDescrNodes");
+//   console.log(taskObjInLocalStorage, "taskObjInLocalStorage");
+
+// }
+// initilizeLocalStorage();
+
+$(document).ready(function () {
+  // Add a listener for click events on the save button.
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
     let time = $(this).parent().attr("id");
     let text = $(this).siblings(".description").val();
-    
-    
-});
-  
-  
-  //HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
+    // localStorage.setItem(time, text);
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+    //retrieve object from ls
+    let taskObject = JSON.parse(localStorage.getItem("taskObject"));
+
+    //   we add/replace new/existing data in the object from LS
+    const newObject = {
+      ...taskObject,
+      [time]: text,
+    };
+
+    console.log(newObject);
+    // push the updated object back to LS
+    localStorage.setItem("taskObject", JSON.stringify(newObject));
+
+    // push into that array
+    // put that new array back into ls
+  });
+
+  // for(let i=0; i<)
+  //function for changing the css style
+  // function changeStyle() {
+  //   let stopTime = $(this).parent().attr("id");
+  //   if (stopTime < currentTime) {
+  //     $(this).removeClass("future");
+  //     $(this).removeClass("present");
+  //     $(this).addClass("past");
+  //   } else if (stopTime === currentTime) {
+  //     $(this).removeClass("past");
+  //     $(this).removeClass("future");
+  //     $(this).addClass("present");
+  //   } else {
+  //     $(this).removeClass("present");
+  //     $(this).removeClass("past");
+  //     $(this).addClass("future");
+  //   }
+  // }
+  // changeStyle();
 });
