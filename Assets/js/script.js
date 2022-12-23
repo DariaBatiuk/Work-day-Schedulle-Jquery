ebd8994fa38b039f8b8a6c0921b74cea306af449
Daria <dariabatiuk5@gmail.com>
2022-12-22 23:43:52 -0400

    wrote changing color function + sm update on LS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,25 +2,10 @@
 
 let currentDate = dayjs().format("dddd, MMM D YYYY");
 $("#currentDay").html(currentDate);
-let hour = document.querySelectorAll(".description");
-let hour9 = document.getElementById("h9");
-console.log(JSON.parse(localStorage.getItem("taskObject")).hour10);
+console.log(currentDate);
 
-
-
-hour9.value = JSON.parse(localStorage.getItem("taskObject")).hour9;
-
-let currentTime = dayjs().format("hh:mm");
-
-// function initilizeLocalStorage() {
-//   // getting data from LS
-//   const taskObjInLocalStorage = JSON.parse(localStorage.getItem("taskObject")) || {};
-
-//   // console.log(objDescrNodes, "objDescrNodes");
-//   console.log(taskObjInLocalStorage, "taskObjInLocalStorage");
-
-// }
-// initilizeLocalStorage();
+let currentTime = dayjs().format("HH");
+console.log(currentTime);
 
 $(document).ready(function () {
   // Add a listener for click events on the save button.
@@ -29,42 +14,86 @@ $(document).ready(function () {
     let time = $(this).parent().attr("id");
     let text = $(this).siblings(".description").val();
 
-    // localStorage.setItem(time, text);
-
     //retrieve object from ls
     let taskObject = JSON.parse(localStorage.getItem("taskObject"));
+		console.log(taskObject);
 
     //   we add/replace new/existing data in the object from LS
     const newObject = {
       ...taskObject,
       [time]: text,
     };
-
     console.log(newObject);
+
     // push the updated object back to LS
     localStorage.setItem("taskObject", JSON.stringify(newObject));
-
-    // push into that array
-    // put that new array back into ls
+		
   });
+	
+//getting data from LS and pushing this into textarea.value
+let hour9 = document.getElementById("h9");
+let hour10 = document.getElementById("h10");
+let hour11 = document.getElementById("h11");
+let hour12 = document.getElementById("h12");
+let hour13 = document.getElementById("h13");
+let hour14 = document.getElementById("h14");
+let hour15 = document.getElementById("h15");
+let hour16 = document.getElementById("h16");
+let hour17 = document.getElementById("h17");
 
-  // for(let i=0; i<)
-  //function for changing the css style
-  // function changeStyle() {
-  //   let stopTime = $(this).parent().attr("id");
-  //   if (stopTime < currentTime) {
-  //     $(this).removeClass("future");
-  //     $(this).removeClass("present");
-  //     $(this).addClass("past");
-  //   } else if (stopTime === currentTime) {
-  //     $(this).removeClass("past");
-  //     $(this).removeClass("future");
-  //     $(this).addClass("present");
-  //   } else {
-  //     $(this).removeClass("present");
-  //     $(this).removeClass("past");
-  //     $(this).addClass("future");
-  //   }
-  // }
-  // changeStyle();
+hour9.value = JSON.parse(localStorage.getItem("taskObject")).hour9;
+hour10.value = JSON.parse(localStorage.getItem("taskObject")).hour10;
+hour11.value = JSON.parse(localStorage.getItem("taskObject")).hour11;
+// if (localStorage.getItem("taskObject").hour10){
+// 	hour10.value = JSON.parse(localStorage.getItem("taskObject")).hour10;
+// }
+// if (localStorage.getItem("taskObject").hour11){
+// 	hour11.value = JSON.parse(localStorage.getItem("taskObject")).hour11;
+// }
+hour12.value = JSON.parse(localStorage.getItem("taskObject")).hour12;
+hour13.value = JSON.parse(localStorage.getItem("taskObject")).hour13;
+hour14.value = JSON.parse(localStorage.getItem("taskObject")).hour14;
+hour15.value = JSON.parse(localStorage.getItem("taskObject")).hour15;
+hour16.value = JSON.parse(localStorage.getItem("taskObject")).hour16;
+hour17.value = JSON.parse(localStorage.getItem("taskObject")).hour17;
+
+  // // for(let i=0; i<)
+	// for (let i=0; i < 8; i ++) { 
+	// 	$('#[i]').description('textarea').text(localStorage.getItem('[i]')); 
+	// }
+
+
+
+	// $(".time-block").each(function () {
+	// 	var stopTime = parseInt($(this).attr("id").split("hour")[1]);
+	// 	console.log(stopTime);
+		
+	// })
+
+
+	  //function for changing the css style
+  function changeStyle() {		
+		let timeBlocks = document.querySelectorAll(".time-block");
+		console.log(timeBlocks);
+		// timeBlocks.forEach(function(el) {
+		for (let i = 0; i < timeBlocks.length; i++) {		
+				var stopTime = parseInt(timeBlocks[i].id.split("hour")[1]);
+				//console.log(timeBlocks[i]);
+				console.log(stopTime);
+				if (stopTime < currentTime) {
+					timeBlocks[i].classList.remove("future");
+					timeBlocks[i].classList.remove("present");
+					timeBlocks[i].classList.add("past");
+				} else if (stopTime == currentTime) {
+					timeBlocks[i].classList.remove("past");
+					timeBlocks[i].classList.remove("future");
+					timeBlocks[i].classList.add("present");
+				} else {
+					timeBlocks[i].classList.remove("present");
+					timeBlocks[i].classList.remove("past");
+					timeBlocks[i].classList.add("future");
+				}
+		}
+  }
+  changeStyle();
 });
