5c77bc8574f0c3e71f7f71b9779fe6a055f2e4d7
Daria <dariabatiuk5@gmail.com>
2023-03-26 19:56:24 -0300

    turned JS into jQuery

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,14 +3,15 @@
 // import dayjs library
 // import dayjs from "dayjs";
 
-let currentDate = dayjs().format("dddd, MMM D YYYY");
-$("#currentDay").html(currentDate);
-console.log(currentDate);
-
-let currentTime = dayjs().format("HH");
-console.log(currentTime);
-
 $(document).ready(function () {
+
+  let currentDate = dayjs().format("dddd, MMM D YYYY");
+  $("#currentDay").html(currentDate);
+  console.log(currentDate);
+
+  let currentTime = dayjs().format("HH");
+  console.log(currentTime);
+
   // Add a listener for click events on the save button.
   $(".saveBtn").on("click", function (event) {
     event.preventDefault();
@@ -33,42 +34,44 @@ $(document).ready(function () {
   });
 
   // getting data from LS and pushing this into textarea.value
-  let hour9 = document.getElementById("h9");
-  let hour10 = document.getElementById("h10");
-  let hour11 = document.getElementById("h11");
-  let hour12 = document.getElementById("h12");
-  let hour13 = document.getElementById("h13");
-  let hour14 = document.getElementById("h14");
-  let hour15 = document.getElementById("h15");
-  let hour16 = document.getElementById("h16");
-  let hour17 = document.getElementById("h17");
+  let hour9 = $("#h9");
+  let hour10 = $("#h10");
+  let hour11 = $("#h11");
+  let hour12 = $("#h12");
+  let hour13 = $("#h13");
+  let hour14 = $("#h14");
+  let hour15 = $("#h15");
+  let hour16 = $("#h16");
+  let hour17 = $("#h17");
 
   // check if property exists before accessing it
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour9")) {
-    hour9.value = JSON.parse(localStorage.getItem("taskObject")).hour9;
+    hour9.val(JSON.parse(localStorage.getItem("taskObject")).hour9);
   }
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour10")) {
-    hour10.value = JSON.parse(localStorage.getItem("taskObject")).hour10;
+    hour10.val(JSON.parse(localStorage.getItem("taskObject")).hour10);
   }
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour11")) {
-    hour11.value = JSON.parse(localStorage.getItem("taskObject")).hour11;
+    hour11.val(JSON.parse(localStorage.getItem("taskObject")).hour11);
   }
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour12")) {
-    hour12.value = JSON.parse(localStorage.getItem("taskObject")).hour12;
+    hour12.val(JSON.parse(localStorage.getItem("taskObject")).hour12);
   }
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour13")) {
-    hour13.value = JSON.parse(localStorage.getItem("taskObject")).hour13;
+    hour13.val(JSON.parse(localStorage.getItem("taskObject")).hour13);
   }
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour14")) {
-    hour14.value = JSON.parse(localStorage.getItem("taskObject")).hour14;
+    hour14.val(JSON.parse(localStorage.getItem("taskObject")).hour14);
   }
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour15")) {
-    hour15.value = JSON.parse(localStorage.getItem("taskObject")).hour15;
+    hour15.val(JSON.parse(localStorage.getItem("taskObject")).hour15);
   }
   if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour16")) {
-    hour16.value = JSON.parse(localStorage.getItem("taskObject")).hour16;
+    hour16.val(JSON.parse(localStorage.getItem("taskObject")).hour16);
 	}
- 
+	if (JSON.parse(localStorage.getItem("taskObject")).hasOwnProperty("hour17")) {
+    hour17.val(JSON.parse(localStorage.getItem("taskObject")).hour17);
+	}
 
 
 
